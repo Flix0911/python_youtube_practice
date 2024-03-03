7fe8524b79b460090312c82390006db307db2a97
Erick Valencia <evalencia0911@gmail.com>
2024-03-03 09:58:01 -0700

    python practice 2

--- intro.py
+++ intro.py
@@ -1,1 +1,24 @@
-print("Hello World")
+# snack case
+# my variable is stored 1. Can also equal an expression (1+1)
+# 1 + "1" is an int + str
+# python is strongly typed, data must be consistent
+#  "1" + "1" is 11, but it's a string, not an int
+# 1.0 is a float, has a decimal
+# Booleans = true or false 
+# 1 + True returns 2
+# (True = 1, False = 0)
+
+# whiz = " whiz"
+# the f allows concatonation
+# String expression
+# my_var = f"Cheese {whiz}"
+
+# boolean expression
+my_var = 5 != 5 or 4 == 4
+# my_var = 5 != 5 and 4 == 4
+
+
+# str - string - str()
+# int - integret - int()
+# float - .X - float()
+print(my_var)
