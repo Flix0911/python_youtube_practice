8bf235ac718b85dc80b28c4379f8f4779cb70860
Erick Valencia <evalencia0911@gmail.com>
2024-03-03 10:24:56 -0700

    python practice 4

---
+++ loop.py
@@ -0,0 +1,8 @@
+# create variable
+counter = 5
+
+while(counter < 5):
+    print("Counter is ", counter)
+    # will add 1 each time
+    # could do counter = counter +1
+    counter += 1
--- myutility.py
+++ myutility.py
@@ -1,24 +1,29 @@
 # control flow / if statements
 # """ allows you to work in multiple lines
+# what if I want to do those over and over and loop it
 
-choice = input("""
-                What do you want to do today? 
-               a) calculate tip
-               b) calculate tax
-               """)
+while(True):
+    choice = input("""
+                    What do you want to do today? 
+                   a) calculate tip
+                   b) calculate tax
+                   c) quit
+                """)
 
 # if takes a boolean expression
 # start a block with :
 # python handles on alignment / indentation
-if(choice == "a"):
-    bill = int(input("How much was the bill?: "))
-    tip = bill * .2
-    print(f"The tip is {tip} for a total bill of {bill + tip}")
+    if(choice == "a"):
+        bill = int(input("How much was the bill?: "))
+        tip = bill * .2
+        print(f"The tip is {tip} for a total bill of {bill + tip}")
 # else if - elif
-elif(choice == "b"):
-    tax = int(input("What is the tax amount?: "))
-    bill = int(input("How much was the bill?: "))
-    print(f"The tax would be {bill * (tax / 100)}")
+    elif(choice == "b"):
+        tax = int(input("What is the tax amount?: "))
+        bill = int(input("How much was the bill?: "))
+        print(f"The tax would be {bill * (tax / 100)}")
+    elif(choice == "c"):
+        break
 # else
-else:
-    print("you did not choose a or b")
+    else:
+        print("you did not choose a, b, or c")
